6a748eb492917899c10ea0bbd6cd6627b0ff6d5c
Anna Nguyenova <nguyenova.anna@gmail.com>
2021-12-18 11:14:42 +0100

    Add project files.

---
+++ GetAreaOfSquare/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetAreaOfSquare
+{
+    interface ITwoDimensionalShape
+    {
+        double GetArea();
+    }
+
+    abstract class Shape : ITwoDimensionalShape
+    {
+        public abstract double GetArea();
+    }
+
+    class Square : Shape
+    {
+        public double Side { get; set; }
+
+
+        public override double GetArea()
+        {
+            return this.Side * this.Side;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new Square() { Side = 3 };
+
+            Console.WriteLine($"Area of the square is {square.GetArea()}.");
+
+            Console.ReadKey();
+        }
+    }
+
+}
