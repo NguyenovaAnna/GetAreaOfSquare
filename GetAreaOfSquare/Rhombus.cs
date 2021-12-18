c0312bfca07b8052b56857e1d2f342a7f3bc943c
Anna Nguyenova <nguyenova.anna@gmail.com>
2021-12-18 12:39:35 +0100

    New class created

---
+++ GetAreaOfSquare/Rhombus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetAreaOfSquare
+{
+    class Rhombus : Shape
+    {
+        public double Side { get; set; }
+
+        public double Height { get; set; }
+
+        public override double GetArea()
+        {
+            return this.Side * this.Height;
+        }
+    }
+}
